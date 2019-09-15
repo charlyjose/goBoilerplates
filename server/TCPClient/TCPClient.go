00a17f3288f2535fdeaa63656c2fc2ec807bf5c8
charlyjose <23473681+charlyjose@users.noreply.github.com>
2019-09-16 02:30:42 +0530

    .

--- server/TCPClient/TCPClient.go
+++ server/TCPClient/TCPClient.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"net/textproto"
+	"os"
+)
 
+func main() {
+	// Create a connection with remote machine
+	conn, connErr := net.Dial("tcp", "[::1]:8081")
+	if connErr != nil {
+		// log.Fatal(connErr)
+		fmt.Printf("Could not connect to %s, target machine may be down or refused connection", conn.RemoteAddr())
+		conn.Close()
+		os.Exit(0)
+	}
+	defer conn.Close()
+
+	fmt.Printf("\nConnected.\n")
+	fmt.Println("Connection Details:")
+	fmt.Printf("Remote Address: %s\n", conn.RemoteAddr())
+	fmt.Printf("Remote Address: %s\n", conn.LocalAddr())
+
+	reader := bufio.NewReader(conn)
+	tp := textproto.NewReader(reader)
+
+	for {
+		// read one line (ended with \n or \r\n)
+		line, _ := tp.ReadLine()
+		fmt.Println(line)
+		if line == "" {
+			break
+		}
+	}
 }
